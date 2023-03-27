678b70f90758ddd602068fbfe486f76c86e7eab0
lpatrik <lpatrik@ad.helsinki.fi>
2023-03-27 14:23:12 +0300

    maksukortti test muokattu ja kassapaate test luotu

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -5,5 +5,27 @@ class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(1000)
 
-    def test_luotu_kortti_on_olemassa(self):
+    def test_luotu_kortti_on_olemassa(self):    
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+    
+    def test_kortille_voi_ladata_rahaa(self):
+        self.maksukortti.lataa_rahaa(2000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 30.00 euroa")
+
+    def test_saldo_ei_vahene_jos_ei_ole_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(9999)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_saldo_vahenee_jos_on_tarpeeksi_rahaa(self):
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 5.00 euroa")
+        self.maksukortti.ota_rahaa(500)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 0.00 euroa")
+    
+    def test_riittaako_rahat(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(9999), False)
+        self.assertEqual(self.maksukortti.ota_rahaa(1), True)
+
