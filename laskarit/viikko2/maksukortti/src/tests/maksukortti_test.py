63da13f090ed769809839416242df970e259c2eb
lpatrik <lpatrik@ad.helsinki.fi>
2023-04-03 19:53:55 +0300

    testien valmistelu

--- laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
+++ laskarit/viikko2/maksukortti/src/tests/maksukortti_test.py
@@ -31,14 +31,12 @@ class TestMaksukortti(unittest.TestCase):
         self.assertEqual(str(kortti), "Kortilla on rahaa 2.00 euroa")
 
     def test_kortille_voi_ladata_rahaa(self):
-    	self.kortti.lataa_rahaa(2500)
-
-    	self.assertEqual(str(self.kortti), "Kortilla on rahaa 35.00 euroa")
+        self.kortti.lataa_rahaa(2500)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 35.00 euroa")
 
     def test_kortin_saldo_ei_ylita_maksimiarvoa(self):
-    	self.kortti.lataa_rahaa(20000)
-
-    	self.assertEqual(str(self.kortti), "Kortilla on rahaa 150.00 euroa")
+        self.kortti.lataa_rahaa(20000)
+        self.assertEqual(str(self.kortti), "Kortilla on rahaa 150.00 euroa")
 
 
     def test_negatiivinen_lataus_ei_muuta_saldoa(self):
---
+++ src/tests/fleet_zero_test.py
@@ -0,0 +1,8 @@
+import unittest
+import pygame
+import os
+from fleet_zero import Spaceship
+
+class TestFleetZero(unittest.TestCase):
+    def test_toimiiko_testi(self):
+        self.asserEqual("here", "here")
