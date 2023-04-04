3667a3f4c55b89e5e6db7c89fdebd31d2a9f826f
lpatrik <lpatrik@ad.helsinki.fi>
2023-04-04 12:09:17 +0300

    ensmmäiset testit

--- src/tests/fleet_zero_test.py
+++ src/tests/fleet_zero_test.py
@@ -1,8 +1,17 @@
 import unittest
 import pygame
 import os
-from fleet_zero import Spaceship
+from fleet_zero import Player_ship
 
 class TestFleetZero(unittest.TestCase):
-    def test_toimiiko_testi(self):
-        self.asserEqual("here", "here")
+    def test_does_test_work(self):
+        self.assertEqual("here", "here")
+
+    def test_does_player_take_dmg(self):
+        
+        self.ship = Player_ship(1,1,1,100)
+        self.image = pygame.image.load(os.path.join("src","assets", "striker.png"))
+        self.ship.hit(20)
+        self.assertEqual(self.ship.health , 80)
+
+    
