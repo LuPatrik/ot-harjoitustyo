57cb7036fc80078f99f22932feb1b442020d7bbd
lpatrik <lpatrik@ad.helsinki.fi>
2023-05-02 18:25:29 +0300

    poistettu vahingossa lisätyt tiedostot

--- game_values.py
+++
@@ -1,6 +0,0 @@
-class GameValues:
-    def __init__(self):
-        self.screen_size = (800,800)
-        self.fps = 60
-        self.name = "Fleet Zero"
-        self.level = 1
--- screen_refresh.py
+++
@@ -1,9 +0,0 @@
-import pygame
-def refresh_screen(screen, player_ship, enemy_ship):
-    pygame.display.update()
-    screen.fill((0,0,0))
-    for bullet in player_ship.bullets:
-        bullet.draw(screen)
-    for enemy in enemy_ship.enemies:
-        enemy.draw(screen)
-    player_ship.draw(screen)
